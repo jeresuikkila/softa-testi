e4e9aa603b82dadee584c98b974860945626a87c
Jere Suikkila <jere@suikkila.com>
2018-09-15 03:21:16 +0300

    Add puzzle

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from pprint import pprint
 
 def hello():
     return 'Hello Wolrd!'
 
+def puzzle(rows=5, columns=5):
+    puzzle = [[0 for i in range(columns)] for j in range(rows)]
+    return puzzle
+
 
 if __name__ == '__main__':
     print(hello())
+    pprint(puzzle(6,6))
+    p = puzzle(2,2)
+    p[0][0] = 1
+    pprint(p)
--- test.py
+++ test.py
@@ -5,5 +5,13 @@ class TestMain(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(main.hello(), "Hello World!")
 
+    def test_puzzle_creation(self):
+        total = 0
+        puzzle = main.puzzle(2,2)
+        puzzle[0][0] = 1
+        for row in range(len(puzzle)):
+            total += sum(puzzle[row])
+        self.assertEqual(total, 1)
+
 if __name__ == '__main__':
     unittest.main()
